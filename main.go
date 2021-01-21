e60a321baf131c87eb056a3c267b9c15f8c62011
Ilya <49365758+subZiro@users.noreply.github.com>
2021-01-21 13:45:42 +0400

    add main.go

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/gofiber/fiber"
+
+	// "/controllers/books"
+	// "/controllers/books"
+	// "./routes"
+	"./database"
+
+	"./routes"
+)
+
+func setupRoutes(app *fiber.App) {
+	// moved from main method
+
+	// api group
+	api := app.Group("/api")
+
+	// give response when at /api
+	api.Get("", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the api endpoint 😉",
+		})
+	})
+
+	// register books endpoints
+	routes.BooksRoute(api.Group("/books"))
+}
+
+func main() {
+	var err error
+	if err = database.ConnectDB("database/sqlite3.db"); err != nil {
+		panic(err)
+	}
+
+	app := fiber.New()
+	setupRoutes(app)
+	err = app.Listen(":8080")
+	// handle error
+	if err != nil {
+		panic(err)
+	}
+}
